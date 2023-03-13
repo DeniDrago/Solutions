21ddc87a3f1b627e6749f5eb1c824a720d440cd0
DeniDrago <Dejhera@gmail.com>
2023-03-13 05:02:26 +0200

    Delete file

--- Division by 0.py
+++
@@ -1,9 +0,0 @@
-def divide_numbers():
-    n1, n2 = int(input('Enter a divisible number: ')), int(input('Enter the divisor of the number: '))
-    try:
-        return f'Division result = {n1 / n2}'
-    except ZeroDivisionError:
-        return f"You can't divide by zero."
-
-
-print(divide_numbers())
